fbd28b51ae3b8f786e0445a2044ba4c6de35c276
Ben <hapiben@users.noreply.github.com>
2017-08-11 10:44:12 +1200

    Basic design (#1)
    * Add foundation js
    
    * Update foundation
    
    * Add mixins and blocks
    
    * Add basic design template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function() {
+  $(document).foundation();
+});
