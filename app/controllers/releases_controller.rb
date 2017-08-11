c700f0708948f5c5b867ba10158f6c1cf6586200
Gustavo Motizuki <gustavo@boost.co.nz>
2017-08-11 15:04:49 +1200

    loading side bar with session stories

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -10,8 +10,13 @@ class ReleasesController < ApplicationController
   end
 
   def set_stories_to_print
-    #TODO: from session cache
-    filter_query = 'id:6817691 OR id:6863121'
+    project_id = params[:project_id]
+
+    session[:stories] ||= {}
+    session[:stories][project_id] ||= []
+    filter_query = session[:stories][project_id].join(' OR ')
+    filter_query = 'id:0' unless filter_query.present?
+
     @stories_to_print = ProjectStory.find(:all, params: { filter: filter_query, project_id: params[:project_id] })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get '/stories/:project_id/:release_id', to: 'pages#stories'
-
   resources :projects, only: :index do
-    resources :releases, only: :index do
-      resources :stories, only: :index do
-        post 'add_to_print', to: 'stories#add_to_print', as: :add_to_print
-        post 'remove_to_print', to: 'stories#remove_to_print', as: :remove_to_print
-      end
+    resources :releases, only: :index
+
+    resources :stories, only: :index do
+      post 'add_to_print', to: 'stories#add_to_print', as: :add_to_print
+      post 'remove_to_print', to: 'stories#remove_to_print', as: :remove_to_print
     end
   end
 end
