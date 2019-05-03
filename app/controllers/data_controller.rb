2919a9080e613ff18680e3831092d4020ee7a36e
Tim Wright <tim@boost.co.nz>
2019-05-03 15:11:37 +1200

    added release id

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,7 +1,7 @@
 class DataController < ApplicationController
   def index
     respond_to do |format|
-      format.json { render :json => Story.where(project_id: params[:project_id]).to_json }
+      format.json { render :json => Story.where(project_id: params[:project_id], release_id: params[:release_id]).to_json }
     end
   end
 end
